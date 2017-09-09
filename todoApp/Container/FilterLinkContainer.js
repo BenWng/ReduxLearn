b0b2649f8a04edd13e1aee55f405f40faf90ee27
Ben Wang <bwang@cogitocorp.com>
2017-09-09 18:28:29 -0400

    FilterLink container uses connect as well

--- todoApp/Container/FilterLinkContainer.js
+++ todoApp/Container/FilterLinkContainer.js
@@ -1,37 +1,58 @@
-class FilterLink extends Component{
-    componentDidMount(){
-        const {store} = this.context;
-        this.unsubsribe=store.subscribe(()=>{
-            this.forceUpdate();
-        });
+// class FilterLink extends Component{
+//     componentDidMount(){
+//         const {store} = this.context;
+//         this.unsubsribe=store.subscribe(()=>{
+//             this.forceUpdate();
+//         });
+//     }
+//
+//     componentWillUnmount(){
+//         this.unsubsribe();
+//     }
+//
+//
+//
+//     render(){
+//
+//         const {store} = this.context;
+//         const {visibilityFilter} = store.getState();
+//         const {filter, children}=this.props;
+//
+//         return (<Link
+//             active = {filter=== visibilityFilter }
+//             children={children}
+//             onClick={() => {
+//                 store.dispatch({
+//                     type: 'SET_VISIBILITY_FILTER',
+//                     filter : filter
+//                 })
+//             }}
+//         />)
+//
+//     }
+// };
+//
+// FilterLink.contextTypes = {
+//     store: React.PropTypes.object
+// }
+
+
+const mapStateToProps = (state, ownProps) => {
+    return {
+        active: ()=>{ return ownProps.filter === state.visibilityFilter; }
     }
-
-    componentWillUnmount(){
-        this.unsubsribe();
-    }
-
-
-
-    render(){
-
-        const {store} = this.context;
-        const {visibilityFilter} = store.getState();
-        const {filter, children}=this.props;
-
-        return (<Link
-            active = {filter=== visibilityFilter }
-            children={children}
-            onClick={() => {
-                store.dispatch({
-                    type: 'SET_VISIBILITY_FILTER',
-                    filter : filter
-                })
-            }}
-        />)
-
-    }
-};
-
-FilterLink.contextTypes = {
-    store: React.PropTypes.object
 }
+
+
+const mapDispatchToProps = (dispatch, ownProps) => {
+    return {
+        onClick: () => {
+            dispatch( {type: 'SET_VISIBILITY_FILTER',
+                filter : ownProps.filter
+            });
+        }
+    }
+}
+
+
+const FilterLink = connect (mapStateToProps, mapDispatchToProps);
